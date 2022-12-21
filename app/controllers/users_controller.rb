a1914015f3055d9901fe441adb8e037c3e188434
Timothy Chang <tchang135@gmail.com>
2022-12-21 15:45:18 -0500

    working on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+    def index 
+        @users = User.all 
+        render :index
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            login(@user)
+            redirect_to user_url(@user)
+        else 
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+    # params[:user][:username], params[:user][:password]
+    def new
+        @user = User.new 
+        render :new
+    end
+
+    def edit
+        
+    end
+
+    def show
+
+    end
+
+    def update 
+
+    end
+
+    def destroy 
+
+    end
+
+
+    def user_params 
+        params.require(:user).permit(:username, :password)
+    end
 end
+
